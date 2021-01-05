f3ef1fbf10ec7567f20139cc28f87517c7cc02aa
Ioana B <buzaulioana@gmail.com>
2021-01-05 14:38:23 +0200

    am adaugat campuri pentru introducere date

---
+++ app/src/main/java/com/example/proiect_bd_tema/CreateAccount.java
@@ -0,0 +1,13 @@
+package com.example.proiect_bd_tema;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
