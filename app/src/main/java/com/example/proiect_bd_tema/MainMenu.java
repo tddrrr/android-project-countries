0b2b3c8c7ae274887de307b8d09f17b5a24edcfd
Ioana B <buzaulioana@gmail.com>
2021-01-06 15:51:00 +0200

    varianta cu nav menu mai functionala si cu chestiile inutile sterse

--- app/src/main/java/com/example/proiect_bd_tema/MainMenu.java
+++ app/src/main/java/com/example/proiect_bd_tema/MainMenu.java
@@ -1,15 +1,7 @@
 package com.example.proiect_bd_tema;
 
 import android.os.Bundle;
-import android.view.Menu;
-
-import com.google.android.material.navigation.NavigationView;
-
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
