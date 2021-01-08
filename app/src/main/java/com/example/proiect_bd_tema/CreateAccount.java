00185bec99a2544d6c040553b002d2eb24fb5182
tddrrr <teodora.birliba@gmail.com>
2021-01-09 01:28:46 +0200

    validations for login and register

--- app/src/main/java/com/example/proiect_bd_tema/CreateAccount.java
+++ app/src/main/java/com/example/proiect_bd_tema/CreateAccount.java
@@ -6,14 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CreateAccount extends AppCompatActivity {
     private Button btnOk;
     public static final String USER_KEY = "user_key";
+    private TextInputEditText tietFirstname;
+    private TextInputEditText tietLastname;
+    private TextInputEditText tietEmail;
+    private TextInputEditText tietPassword;
     private  Intent intent;
 
     private void initComp(){
         btnOk=findViewById(R.id.create_acc_btn_ok);
+        tietFirstname = findViewById(R.id.create_acc_tied_firstName);
+        tietLastname = findViewById(R.id.create_acc_tied_lastName);
+        tietEmail = findViewById(R.id.create_acc_tiet_email);
+        tietPassword = findViewById(R.id.create_acc_tiet_password);
         btnOk.setOnClickListener(addSaveClickEvent());
     }
     @Override
@@ -24,7 +35,23 @@ public class CreateAccount extends AppCompatActivity {
         intent=getIntent();
     }
     private boolean validate(){
-        return  true;
+        if (tietFirstname.getText().toString().length() < 2) {
+            Toast.makeText(getApplicationContext(), "Enter a valid name", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (tietLastname.getText().toString().length() < 2) {
+            Toast.makeText(getApplicationContext(), "Enter a valid name", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!tietEmail.getText().toString().trim().contains("@")){
+            Toast.makeText(getApplicationContext(), "Enter a valid email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(tietPassword.getText().toString().length()<5){
+            Toast.makeText(getApplicationContext(), "Enter a valid password larger than 5 characters", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
     private View.OnClickListener addSaveClickEvent() {
         return new View.OnClickListener() {
@@ -45,8 +72,12 @@ public class CreateAccount extends AppCompatActivity {
         };
     }
     private User createUser(){
-        User a = new User("a", "a","a","a");
-        return a;
+        String firstName = tietFirstname.getText().toString();
+        String lastName = tietLastname.getText().toString();
+        String email = tietEmail.getText().toString();
+        String password = tietPassword.getText().toString();
+        User user = new User(firstName, lastName, email, password);
+        return user;
     }
 
 }
--- app/src/main/java/com/example/proiect_bd_tema/Login.java
+++ app/src/main/java/com/example/proiect_bd_tema/Login.java
@@ -6,17 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
     private Button btnOk;
+    private TextInputEditText tietEmail;
+    private TextInputEditText tietPassword;
 
     private void initComp(){
         btnOk = findViewById(R.id.btn_login_ok);
+        tietEmail = findViewById(R.id.login_email);
+        tietPassword = findViewById(R.id.login_password);
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainMenu.class);
-                startActivity(intent);
+                if (validateComponents()) {
+                    Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                    startActivity(intent);
+                }
             }
         });
     }
@@ -25,6 +34,17 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         initComp();
-        //buna buna
+    }
+
+    private boolean validateComponents() {
+        if(tietEmail.getText().toString().isEmpty() || tietPassword.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), R.string.enter_credentials, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!tietEmail.getText().toString().contains("@")){
+            Toast.makeText(getApplicationContext(), R.string.invalid_email, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
