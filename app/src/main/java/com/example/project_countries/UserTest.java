c477b9594513d32b19982c17941e78dc7bc0e2b8
Ioana B <buzaulioana@gmail.com>
2021-01-09 18:40:14 +0200

    adaugat entitati

--- app/src/main/java/com/example/project_countries/UserTest.java
+++ app/src/main/java/com/example/project_countries/UserTest.java
@@ -1,14 +1,14 @@
 package com.example.project_countries;
 
-import androidx.room.Embedded;
-import androidx.room.Junction;
-import androidx.room.Relation;
+        import androidx.room.Embedded;
+        import androidx.room.Junction;
+        import androidx.room.Relation;
 
-import com.example.project_countries.database.entities.ResultTest;
-import com.example.project_countries.database.entities.Test;
-import com.example.project_countries.database.entities.User;
+        import com.example.project_countries.database.entities.ResultTest;
+        import com.example.project_countries.database.entities.Test;
+        import com.example.project_countries.database.entities.User;
 
-import java.util.List;
+        import java.util.List;
 
 public class UserTest {
     @Embedded public User user;
